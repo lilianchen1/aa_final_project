88bd64d7353425e2956ffd3933ffbf9356e531f8
Yiling <yilingchen1@gmail.com>
2015-04-08 17:30:53 -0700

    basic css

--- app/assets/javascripts/views/question_index_item.js
+++ app/assets/javascripts/views/question_index_item.js
@@ -14,7 +14,7 @@ NoPhenotype.Views.QuestionIndexItem = Backbone.View.extend({
     });
     this.$el.html(content);
     this.model.tags().each(function(tag) {
-      var tagLink = "<a href='#/tags/" + tag.id + "'>" + tag.get('name') + "</a>  ";
+      var tagLink = "<a href='#/tags/" + tag.id + "' class='tags'>" + tag.get('name') + "</a>  ";
       this.$("div.tags").append(tagLink);
     }.bind(this));
     return this;
--- app/assets/javascripts/views/tags_index.js
+++ app/assets/javascripts/views/tags_index.js
@@ -5,9 +5,19 @@ NoPhenotype.Views.TagsIndex = Backbone.View.extend({
     this.listenTo(this.collection, "sync", this.render);
   },
 
+  events: {
+    "keyup input.tag-search": "renderMatchedTag"
+  },
+
   render: function() {
     var content = this.template({tags: this.collection});
     this.$el.html(content);
     return this;
+  },
+
+  renderMatchedTag: function(event) {
+    event.preventDefault();
+    var attrs = this.$(":input").serializeJSON();
+    // implement search...
   }
 });
