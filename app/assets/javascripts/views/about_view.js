5a02e91d410fb6bc62a48d1b220d570efcc536f3
Yiling <yilingchen1@gmail.com>
2015-04-16 23:11:48 -0700

    css

--- app/assets/javascripts/views/about_view.js
+++ app/assets/javascripts/views/about_view.js
@@ -11,12 +11,14 @@ NoPhenotype.Views.AboutView = Backbone.View.extend({
     this.$el.html(content);
     // $("#content").addClass("fade");
     $("#content").css("background", "gray");
+    //append a div with opacity ~0.5 and z index greater than body but less than modal?
     return this;
   },
 
   closeAboutView: function() {
     // $("#content").removeClass("fade");
     $("#content").css("background", "");
+    //remove that div?
     this.remove();
   }
 });
